bb4a0041b1a64d47f2a1572025cc7a5bb1505284
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-09-09 17:03:12 +0530

    updated

---
+++ bmwserviceapp/serviceapp/src/atoms/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+
+import BmwLogo from '../../assets/bmw-logo.png'
+const Logo = () => {
+    return (
+        <img src={BmwLogo} alt="Logo" style={{ width: '100px', height: 'auto' }}/>
+    )
+}
+
+export default Logo;
